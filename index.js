cbf48af1b3687ad8f59d7a146bc8052004dd67be
Jonathan Ryan Steadman <52569279+ToothedTomb@users.noreply.github.com>
2021-07-31 16:07:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ buttons.map( button => {
                 try{
                     display.innerText = eval(display.innerText);
                 } catch {
-                    display.innerText = "ERROR FOUND :(."
+                    display.innerText = "ERROR FOUND :("
                 }
                 break;
             case '←':
